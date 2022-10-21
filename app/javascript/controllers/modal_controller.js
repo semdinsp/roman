89b7730036f86742978227baa38745f2780bfadf
Scott Sproule <scott.sproule@ficonab.com>
2022-10-21 23:29:15 +0800

    update for tailinw

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,17 +1,31 @@
 import { Controller } from "@hotwired/stimulus";
  
 export default class extends Controller {
-    open() {
-        document.body.classList.add("modal-open");
-        this.element.setAttribute("style", "display: block;");
-        this.element.classList.add("show");
-        document.body.innerHTML += '<div class="modal-backdrop fade show"></div>';
-    }
+    static targets = ["modal"]
+
+    //open() {
+    //    document.body.classList.add("modal-open");
+    //    this.element.setAttribute("style", "display: block;");
+    //    this.element.classList.add("show");
+    //    document.body.innerHTML += '<div class="modal-backdrop fade show"></div>';
+    //}
  
-    close() {
-        document.body.classList.remove("modal-open");
-        this.element.removeAttribute("style");
-        this.element.classList.remove("show");
-        document.getElementsByClassName("modal-backdrop")[0].remove();
-    }
+   // close() {
+    //    document.body.classList.remove("modal-open");
+     //   this.element.removeAttribute("style");
+     //   this.element.classList.remove("show");
+     //   document.getElementsByClassName("modal-backdrop")[0].remove();
+   // }
+
+    open(event) {
+        event.preventDefault();
+    
+        this.modalTarget.showModal();
+      }
+    
+      close(event) {
+        event.preventDefault();
+    
+        this.modalTarget.close();
+      }
 }
